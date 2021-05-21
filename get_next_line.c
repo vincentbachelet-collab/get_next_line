103849234cabfb6f88653cf6c72c73f6201838c6
Slpn <shaan99@live.fr>
2021-05-22 00:51:08 +0200

    work

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snarain <snarain@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/16 20:06:22 by snarain           #+#    #+#             */
+/*   Updated: 2021/05/22 00:41:09 by snarain          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#define  BUFFER_SIZE
+
+int	get_next_line(int fd,char **line)
+{
+	int		i;
+	char	buffer[BUFFER_SIZE];
+
+	while ()
+	{
+		read(fd,buffer,BUFFER_SIZE);
+	}
+	return 0;
+}
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snarain <snarain@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/17 21:23:34 by snarain           #+#    #+#             */
+/*   Updated: 2021/05/22 00:37:22 by snarain          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+void	ft_putstr_fd(char *s, int fd)
+
+#endif
