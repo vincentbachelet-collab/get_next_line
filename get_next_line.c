63da74c19c4669096365b7d516eb2d1ac3054d69
Slpn <75524609+Slpn@users.noreply.github.com>
2021-05-22 21:57:46 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,29 +12,14 @@
 #include "get_next_line.h"
 #define  BUFFER_SIZE 4096
 
-int	count_line(char **line)
-{
-	int	c_line;
-
-	c_line = 0;
-	while (*line != EOF)
-	{
-		if (line == '\n')
-			c_line++;
-		line++;
-	}
-	return (c_line);
-}
-
 int	get_next_line(int fd,char **line) // **line = on doit remplir le **line grace au fd qu'on nous envoie.
 {
 	int		i;
-	int 	len_l;
 	char	*ret;
 	char	buffer[BUFFER_SIZE];
 
-	len_l = count_l(**line);
 //	ret = read(fd,buffer,BUFFER_SIZE);
+	while ()
 	{
 
 	}
